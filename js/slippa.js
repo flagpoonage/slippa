15bd1af80673fc83e6e1f9ed687dd2149eba5d65
flagpoonage <pokinoice@live.com>
2014-11-24 12:59:55 +1300

    V 0.2 added
    Event control throughout objects.

--- js/slippa.js
+++ js/slippa.js
@@ -1,33 +1,115 @@
-(function(){
-	slippa = function(obj){
-		var slippaFunction = typeof obj._constructor === 'function';	
+/* SLippa Library - James Hay 2014 - v0.2*/
+;(function(){
+	slippa = function(o){
+		var sFn = typeof o._constructor === 'function';	
+		var sCp = slippa._configureProxy;
+		var sCr = slippa._configureResponder;		
 
-		proto = slippaFunction ? obj._constructor : function(attributes) { 
-			if(typeof attributes !== 'undefined'){
-				this.attributes = attributes;
-			}
-			this.init(); 
-		};
+		if(sFn){
+			var SlippaObject = (function(){
+				function SlippaObject(){
+					sCp.call(this);
 
-		if(typeof obj.init === 'undefined'){
-			proto.prototype.init = function() {};
+					if(typeof this.respond === 'function'){
+						sCr.call(this);
+					}
+
+					this._constructor.apply(this, arguments);
+					this.init();
+				}
+
+				return SlippaObject;
+			})();
+		}
+		else{
+			var SlippaObject = (function(){
+				function SlippaObject(){
+					sCp.call(this);
+
+					if(typeof this.respond === 'function'){
+						sCr.call(this);
+					}
+
+					this.init();
+				}
+
+				return SlippaObject;
+			})();
 		}
 
-		for(var i in obj){
-			if(obj.hasOwnProperty(i)){
-				if(i === 'slippa' && slippaFunction){
+		p = SlippaObject;
+		var pp = p.prototype;
+
+		if(typeof o.init === 'undefined'){
+			pp.init = function() {};
+		}
+
+		for(var i in o){
+			if(o.hasOwnProperty(i)){
+				if(i === 'slippa' && sFn){
 					continue;
 				}
-				proto.prototype[i] = obj[i];
+
+				pp[i] = o[i];
 			}
 		}		
 
 		if(slippa.initialized){
-			proto.prototype.events = slippa.events;
+			pp.appevents = slippa.events;
+			pp.events = new slippa.EventObject();
 		}
 
-		return proto;
+		return p;
+	};
+
+	slippa.config = {
+		respondSizes: []
+	};
+
+	// TODO: Wire this up.
+	slippa.configure = function(config){
+		this.config.respondSizes = config.respondSizes || [];
+		if(this.config.respondSizes.length > 0){
+
+		}
+	};
+
+	slippa._configureResponder = function(){
+		this.isMobile = false;
+		this.currentSize = -1;
+		this._responderInitialized = false;
+		this.appevents.on('size:respond', slippa._respondInternal.call(this), this);
+	};
+
+	slippa._respondInternal = function(){
+		return function(width){
+			this.previousSize = this.currentSize;
+			this.currentSize = width;
+			this.isMobile = this.currentSize < 768;
+
+			this.respond({
+				fromMobile: this.previousSize < 768 && this.currentSize >= 768,
+				toMobile: (this.previousSize >= 768 || this.previousSize === -1) && this.currentSize < 768
+			});
+		}
+	};
+
+	slippa._configureProxy = function(){
+		this.proxy = {};
+		for(var i in this){
+			if(typeof this[i] === 'function'){
+				this.proxy[i] = slippa.proxy.call(this, this[i]);
+			}
+		}		
 	}
+
+	slippa.proxy = function(fn){
+		return (function(self){
+			return function(){
+				fn.apply(self, arguments);
+			}
+		})(this);
+	};
 
 	slippa.initialized = false;
 
@@ -58,6 +140,25 @@
 			for(var i = 0; i < this.listeners.length; i++){
 				this.listeners[i].callback.call(this.listeners[i].context, data);
 			}
+		},
+
+		ask: function(data){
+			var res = [];
+			res.result = true;
+			for(var i = 0; i < this.listeners.length; i++){
+				res.push({
+					result: this.listeners[i].callback.call(this.listeners[i].context, data),
+					content: this.listeners[i].context});
+			}			
+
+			for(var i = 0; i < res.length; i++){
+				if(!res[i].result){
+					res.result = false;
+					break;
+				}
+			}
+
+			return res;
 		}
 	});
 
@@ -81,11 +182,11 @@
 			var signal = this.signals[signalKey];
 			if(typeof signal !== 'undefined'){
 				signal.removeListener(context, callback);
-			}
 
-			if(signal.listeners.length === 0){
-				this.signals[signalKey] = undefined;
-				delete this.signals[signalKey];
+				if(signal.listeners.length === 0){
+					this.signals[signalKey] = undefined;
+					delete this.signals[signalKey];
+				}
 			}
 		},
 
@@ -94,8 +195,16 @@
 			if(typeof signal !== 'undefined'){
 				signal.dispatch(data);
 			}		
-		}
+		},
 
+		ask: function(signalKey, data){
+			var signal = this.signals[signalKey];
+			if(typeof signal !== 'undefined'){
+				return signal.ask(data);
+			}		
+
+			return true;
+		}
 	});
 
 	slippa.events = new slippa.EventObject();
